17caf5f34c530f01fcff0533274a3fc302b5ee53
bear (Mike Taylor) <bear@andyet.net>
2015-09-23 00:38:55 -0400

    add some new metadata items, update copyright year, bump version and add Kartik to the author entry

--- ronkyuu/__init__.py
+++ ronkyuu/__init__.py
@@ -1,13 +1,13 @@
 #/usr/bin/env python
 
-VERSION = (0, 2, 11, '')
-
-__author__    = 'Mike Taylor'
-__contact__   = 'bear@bear.im'
-__copyright__ = 'Copyright (c) 2013-2014 by Mike Taylor and Kartik Prabhu'
-__license__   = 'MIT'
-__version__   = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
-
+__author__       = 'Mike Taylor and Kartik Prabhu'
+__email__        = 'bear@bear.im'
+__copyright__    = 'Copyright (c) 2013-2015 by Mike Taylor and Kartik Prabhu'
+__license__      = 'MIT'
+__version__      = '0.3'
+__url__          = 'https://github.com/bear/ronkyuu'
+__download_url__ = 'https://pypi.python.org/pypi/ronkyuu'
+__description__  = 'Webmention Manager'
 
 from tools import discoverConfig
 from webmention import findMentions, findEndpoint, discoverEndpoint, sendWebmention
